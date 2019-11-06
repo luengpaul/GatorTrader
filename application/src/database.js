2ca9597c7107a4ea8db85e26b71f98bdd34f1ccd
Ibraheem Chaudry <ichaudry@mail.sfsu.edu>
2019-11-06 12:39:17 -0800

    Added a code to create local database connection for offline development needs

--- application/src/database.js
+++ application/src/database.js
@@ -1,14 +1,27 @@
 const mysql = require('mysql')
 
 var connection = () => {
+    //This code commented out is for connection to remote database. Comment out as needed.
+    
+    // var db = mysql.createConnection({
+    //     host: 'gatortrader.cdnacoov8a86.us-west-1.rds.amazonaws.com',
+    //     port: '3306',
+    //     user: 'admin',
+    //     password: 'csc648_team10',
+    //     //working database name, actual database is 'gatortrader'
+    //     database: 'gatortrader_test'
+    // });
+
+    //This connection is when running local host mysql instance
     var db = mysql.createConnection({
-        host: 'gatortrader.cdnacoov8a86.us-west-1.rds.amazonaws.com',
+        host: '127.0.0.1',
         port: '3306',
-        user: 'admin',
-        password: 'csc648_team10',
+        user: 'root',
+        password: 'aprilhaj27',
         //working database name, actual database is 'gatortrader'
         database: 'gatortrader_test'
     });
+
     db.connect(function (err) {
         if (err) throw err
     })
