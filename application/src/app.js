dee3f424c0d785ddfd6738c6830ec1d200fe93ea
Beers15 <alexbeerz@yahoo.com>
2019-11-05 20:10:09 -0800

    Moved database connection code to its own file to be imported to other js files as needed. Also moved category initialization from database related code to said file.

--- application/src/app.js
+++ application/src/app.js
@@ -4,91 +4,35 @@ const mysql = require('mysql')
 const bodyParser = require('body-parser')
 const routes = require('./routes/routes')
 const aboutRoutes = require('./routes/aboutPgRoutes')
+const db = require('./database')
+try {
+    var dbConnection = db.connection()
+} catch (err) {
+    console.log(err)
+}
+
 
 //configures ejs as templating language
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+//add middleware layers required for application (static file serving, etc)
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/views')));
+app.use('/', routes)
+app.use('/', aboutRoutes)
 
-//allows paring incoming request bodies in a middleware before handlers
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-//create connection to application database con
-var db = mysql.createConnection({
-    host: 'gatortrader.cdnacoov8a86.us-west-1.rds.amazonaws.com',
-    port: '3306',
-    user: 'admin',
-    password: 'csc648_team10',
-    //working database name, actual database is 'gatortrader'
-
-    database: 'gatortrader_test'
-});
-
-db.connect(function (err) {
-    if (err) throw err
-    console.log("Connected!")
-})
-
-//temp solution since dynamically getting all categories from database doesn't rerender
-const types = ['Appliances', 'Electronics', 'Clothes', 'Furniture', 'Tutoring', 'Textbooks']
-
-//routes
-app.get("/", (req, res) => {
-    //res.sendFile(path.join(__dirname, '/views', 'home.html'))
-    res.render('home', {
-        searchResult: "",
-        categories: types
-    })
-    console.log(types)
-})
-
-
-app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', '/about.html'))
-})
-
-app.get("/team/ibraheem", (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/team', '/ibraheem.html'))
-})
-
-app.get("/team/tom", (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/team', '/tom.html'))
-})
-
-app.get("/team/alexander", (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/team', '/alexander.html'))
-})
-
-
-db.query("SELECT * FROM category", (err, result) => {
-    if (err) {
-        console.log(err)
-    } else {
-        if (types.length == 0) {
-            for (let i = 0; i < result.length; i++) {
-                types.push(result[i].type)
-            }
-        }
-    }
-})
+var types = db.initCategories()
 
 //used for test post that gets value from the protype homepage search bar
 app.post('/', function (req, res) {
-
-    if(!db._connectCalled) {
-        db.connect()
-    }
-  
     //deal with category result here later
     console.log("value returned from search entry is (" + req.body.searchEntry + ")")
 
     //if category was selected output all items for that category
     if(types.includes(req.body.searchEntry)) {
-            db.query("SELECT * FROM item WHERE category=?", [req.body.searchEntry], (err, result) => {
+            dbConnection.query("SELECT * FROM item WHERE category=?", [req.body.searchEntry], (err, result) => {
             if (err) {
                 console.log(err)
             } 
@@ -100,7 +44,7 @@ app.post('/', function (req, res) {
         })
     }
     else if (!req.body.searchEntry) {
-        db.query("SELECT * FROM item", (err, result) => {
+        dbConnection.query("SELECT * FROM item", (err, result) => {
             if (err) {
                 console.log(err)
             } else {
@@ -113,7 +57,7 @@ app.post('/', function (req, res) {
         })
     } else {
         //else only output the item that the user entered
-        db.query("SELECT * FROM item WHERE name like '%" + req.body.searchEntry+ "%'", (err, result) => {
+        dbConnection.query("SELECT * FROM item WHERE name like '%" + req.body.searchEntry+ "%'", (err, result) => {
             if (err) {
                 console.log(err)
             } else {
--- application/src/routes/routes.js
+++ application/src/routes/routes.js
@@ -1,35 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const mysql = require('mysql')
+const express = require('express'), router = express.Router()
+const db = require('../database')
 
-var db = mysql.createConnection({
-    host: 'gatortrader.cdnacoov8a86.us-west-1.rds.amazonaws.com',
-    port: '3306',
-    user: 'admin',
-    password: 'csc648_team10',
-    //working database name, actual database is 'gatortrader'
-    database: 'gatortrader_test'
-})
-db.connect(function (err) {
-    if(err) throw err
-})
-
-var types = []
-db.query("SELECT * FROM category", (err, result) => {
-    if (err) {
-        console.log(err)
-    } else {
-        if (types.length == 0) {
-            for (let i = 0; i < result.length; i++) {
-                types.push(result[i].type)
-            }
-        }
-    }
-})
+var types = db.initCategories()
 
 router.get("/", (req, res) => {
-    //necessary to get categories to appear before page is refreshed
-    res.setTimeout(500, () => {
+    //timeout necessary to get categories to appear before page is refreshed
+    res.setTimeout(200, () => {
         res.render('home', {
             searchResult: "",
             categories: types
